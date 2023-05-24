a9d25fb37806baadc31322854f3f421b84e6bbdc
victor toledo <querynetworking@gmail.com>
2023-05-23 23:20:19 -0300

    creacion de rutas

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const debug = require("debug")("app:main");
 
 const { Config } = require("./src/config/index");
+const { ProductsAPI } = require('./src/products/index');
 
 const app = express();
 
 app.use(express.json());
 
 // modulos
+ProductsAPI(app);
 
 app.listen(Config.port, () => {
     debug(`Servidor escuchando en el puerto ${Config.port}`);
---
+++ src/products/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+
+module.exports.ProductsAPI = (app) => {
+    router
+        .get("/", (req, res) => {}) // http://localhost:3000/api/products/
+        .get("/:id", (req, res) => {}) // http://localhost:3000/api/products/id
+        .post("/", (req, res) => {});
+    
+    app.use("/api/products", router);
+};
