4e088367bb19e0cafd3c66756353b21616b721a7
victor toledo <querynetworking@gmail.com>
2023-05-26 00:50:24 -0300

    creacion de usuario

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const debug = require("debug")("app:main");
 
 const { Config } = require("./src/config/index");
 const { ProductsAPI } = require('./src/products/index');
+const { UsersAPI } = require("./src/users/index");
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 
 // modulos
 ProductsAPI(app);
+UsersAPI(app);
 
 app.listen(Config.port, () => {
     debug(`Servidor escuchando en el puerto ${Config.port}`);
